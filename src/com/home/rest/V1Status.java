97195d0722ea4d2d8005df34032dff58634338ae
Akhil Nambiar <Akhil Nambiar>
2017-03-28 07:46:19 +0530

    End of chapter 2

--- src/com/home/rest/V1Status.java
+++ src/com/home/rest/V1Status.java
@@ -1,6 +1,7 @@
 package com.home.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -8,10 +9,19 @@ import javax.ws.rs.core.MediaType;
 @Path("/v1/status")
 public class V1Status {
 	
+	private static final String api_version = "1.0";
+
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle(){
 		return "<p>Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Version :</p>" + api_version;
 	}
 	
 }
