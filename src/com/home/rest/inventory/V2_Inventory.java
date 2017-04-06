ca921f552632fd8c99fa0fa9abce2e07bb0d5308
Akhil Nambiar <Akhil Nambiar>
2017-04-06 08:00:29 +0530

    Post

--- src/com/home/rest/inventory/V2_Inventory.java
+++ src/com/home/rest/inventory/V2_Inventory.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,6 +15,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 
 import com.home.dao.OracleHome;
@@ -200,4 +203,82 @@ public Response returnBrandPartsPathTwo(@PathParam("brand")String brand , @PathP
 
 }	
 	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON) 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Path("/insert")
+public Response InsertIntoValues(String formdata){
+		
+		System.out.println("Data"+ formdata);
+		
+	//	System.out.println("Here brand:" + brand + " " + item_number + "?");
+		
+		System.out.println("Here");
+		
+		PreparedStatement query = null;
+		
+		String returnString = null;
+		Connection conn = null;
+		
+		//Response rb = null;
+		
+		try{
+			ObjectMapper objmapper = new ObjectMapper();
+			ItemEntry itemEntry = objmapper.readValue(formdata, ItemEntry.class);
+			
+			conn = OracleHome.DataSourceConn().getConnection();
+			query = conn.prepareStatement("INSERT INTO PC_PARTS (PC_PARTS_PK, PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL) VALUES (?,?,?,?,?)");
+			
+			//query.setString(1, brand);
+			//query.setString(2, item_number);
+			
+			query.setString(1, itemEntry.PC_PARTS_PK);
+			query.setString(2, itemEntry.PC_PARTS_TITLE);
+			query.setString(3, itemEntry.PC_PARTS_CODE);
+			query.setString(4, itemEntry.PC_PARTS_MAKER);
+			query.setString(5, itemEntry.PC_PARTS_AVAIL);
+			
+			query.executeUpdate();
+			
+			/*System.out.println("Here as well");
+			
+			ToJson converter = new ToJson();
+			JSONArray array = new JSONArray(); 
+			
+			array = converter.toJSONArray(rs);
+			query.close();*/
+			
+			//returnString = array.toString();
+			
+			//rb = Response.ok(returnString).build();
+}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(conn!=null){
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return Response.status(500).entity("Server was unable to process the request").build();
+				}
+			}
+		}
+		
+		return Response.ok(returnString).build();
+
+}	
+	
+}
+
+class ItemEntry{
+	
+	public String PC_PARTS_PK;
+	public String PC_PARTS_TITLE;
+	public String PC_PARTS_CODE;
+	public String PC_PARTS_MAKER;
+	public String PC_PARTS_AVAIL;
 }
